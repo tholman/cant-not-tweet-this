96f9c002499572bb6f896a0600f2211c1c0b1660
Tim <timothy.w.holman@gmail.com>
2014-07-19 19:42:19 -0400

    meta data

--- js/cant-not-tweet-this.js
+++ js/cant-not-tweet-this.js
@@ -21,10 +21,15 @@ function App() {
 
     	theButton = document.querySelector( '.the-button' );
     	spans     = document.querySelectorAll( '.background span' );
-    	mouse.x   = document.innerWidth / 2;
-    	mouse.y   = document.innerHeight / 2;
+    	mouse.x   = window.innerWidth / 2;
+    	mouse.y   = window.innerHeight / 2;
 
     	bindElements();
+
+    	// Position button for before interactions.
+    	theButton.style.WebkitTransform = "translate3d(" + mouse.x + "px, " + (mouse.y - 150) + 'px, 0px)';
+    	theButton.style.MozTransform = "translate3d(" + mouse.x + "px, " + (mouse.y - 150) + 'px, 0px)';
+    	theButton.style.transform = "translate3d(" + mouse.x + "px, " + (mouse.y - 150) + 'px, 0px)';
     }
 
     var bindElements = function() {
@@ -48,13 +53,22 @@ function App() {
 
     	mouse.x = e.pageX;
     	mouse.y = e.pageY;
+
     	theButton.style.pointerEvents = "none";
     	theButton.style.WebkitTransform = "translate3d(" + mouse.x + "px, " + mouse.y + 'px, 0px)';
+    	theButton.style.MozTransform = "translate3d(" + mouse.x + "px, " + mouse.y + 'px, 0px)';
+    	theButton.style.transform = "translate3d(" + mouse.x + "px, " + mouse.y + 'px, 0px)';
+    	
+    	// Allow clicking of the button, when its not moving.
     	clearTimeout( timeout );
     	timeout = setTimeout( allowClick, 100 );
     }
-
 }
 
 var app = new App();
 app.init();
+
+// Omg meta, you're not reading a comment about comments.
+console.log( "Hey there! Checkin out that console eh?" );
+console.log( "You'll be happy to know that this project (as useful as it is) is open sourced - http://github.com/tholman/cant-not-tweet-this" );
+console.log( "You can also check out my other random works @ http://tholman.com" );
