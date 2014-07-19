3b63ff60b5552f578de5b26ca661fe2ceea01ec9
Tim <timothy.w.holman@gmail.com>
2014-07-19 14:19:26 -0400

    initial project

---
+++ js/cant-not-tweet-this.js
@@ -0,0 +1,18 @@
+/*!
+ * cant-not-tweet-this
+ *
+ * MIT licensed
+ * Copyright (C) 2013 Tim Holman, http://tholman.com
+ */
+
+/*********************************************
+ *
+ *********************************************/
+
+function app() {
+
+    this.init = function() {
+
+    }
+
+}
